1e35c78f203391b9c7c8188015814c38fbd0d59c
bogdannechyporenko <bnechyporenko@xebia.com>
2014-12-07 22:52:06 +0100

    Added missing libraries for build.cmd

--- build.cmd
+++ build.cmd
@@ -12,5 +12,8 @@ go get github.com/acierto/unzipit
 go get github.com/c4pt0r/cfg
 go get github.com/acierto/go-jira-client
 go get launchpad.net/goyaml
+go get github.com/GeertJohan/go.rice
+go get github.com/GeertJohan/go.rice/rice
+go get github.com/stretchr/testify/assert
 
 go build
